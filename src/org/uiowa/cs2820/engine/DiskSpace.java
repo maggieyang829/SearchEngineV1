68827514e266423a60eabf7ed15261dc5acaeeb6
kreyes29 <khashi-reyes@uiowa.edu>
2014-11-02 00:06:44 -0500

    Wrote write and read method along with size and isEmpty methods for DispSpace.

--- src/org/uiowa/cs2820/engine/DiskSpace.java
+++ src/org/uiowa/cs2820/engine/DiskSpace.java
@@ -1,12 +1,45 @@
 package org.uiowa.cs2820.engine;
 
-public class DiskSpace {
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
-	public static byte[] read(long area){
-		
+public class DiskSpace {
+	private static File file;
+	
+	public static void write(long area, byte[] b) throws IOException{
+		if(file.exists()) {
+			RandomAccessFile rafw = new RandomAccessFile(file, "rw"); //creates random access file
+			rafw.seek(area); //sets pointer in file to area
+			rafw.setLength(DiskSpace.size() + b.length); //pre-allocates space for writing
+			rafw.write(b); //writes the byte array to the file
+			rafw.close();
+		}
+		//if file doesn't exist, create file and recurse
+		else {
+			file = new File("byteArray.txt");
+			write(area, b);
+		}	
 	}
 	
-	public static void write(long area, byte[] b) throws Exception{
-		
+	public static byte[] read(long area) throws IOException{
+		RandomAccessFile rafr = new RandomAccessFile(file, "rw");
+		byte[] B = new byte[DiskSpace.size()]; //creates a new byte array
+		rafr.seek(area); //sets pointer in file to area needed
+		rafr.readFully(B); //reads from pointer to end of file and writes to byte array
+		rafr.close();
+		return B;
+	}
+	
+	//returns the size of the file
+	public static int size() {
+		int fileSize = (int) file.length();
+		return fileSize;	
+	}
+	
+	//checks if the file is empty, not sure if really needed
+	public static boolean isEmpty() {
+		if (DiskSpace.size() == 0) return true;
+		else return false;
 	}
 }
