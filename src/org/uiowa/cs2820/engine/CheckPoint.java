16a51730577c254ba5ee31b9a1cc8b9527fe99ba
Mitchell Kinney <mknney@CS-S129.iowa.uiowa.edu>
2014-11-03 21:37:03 -0600

    Added shell of checkpoint class. Left Allocate as is

---
+++ src/org/uiowa/cs2820/engine/CheckPoint.java
@@ -0,0 +1,18 @@
+package org.uiowa.cs2820.engine;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.BitSet;
+
+public class CheckPoint {
+	
+	public static boolean save(BitSet bitset){
+		return true;
+	}
+	
+	public static BitSet restore(File f){
+		
+	}
+
+}
