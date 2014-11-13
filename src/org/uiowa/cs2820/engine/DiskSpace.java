7beb7bf5716d6e9718c168567336e0c3c8566d7d
Khashi Reyes <khashi-reyes@uiowa.edu>
2014-11-13 14:28:20 -0600

    Update DiskSpace.java
    Edited all methods

--- src/org/uiowa/cs2820/engine/DiskSpace.java
+++ src/org/uiowa/cs2820/engine/DiskSpace.java
@@ -5,40 +5,37 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class DiskSpace {
-	private static File file;
-	
+	private static File file = LinearMemoryDatabase.getDataStorage();
 	public static void write(long area, byte[] b) throws IOException{
-		if(file.exists()) {
-			RandomAccessFile rafw = new RandomAccessFile(file, "rw"); //creates random access file
-			rafw.seek(area); //sets pointer in file to area
-			rafw.setLength(DiskSpace.size() + b.length); //pre-allocates space for writing
-			rafw.write(b); //writes the byte array to the file
-			rafw.close();
-		}
-		//if file doesn't exist, create file and recurse
-		else {
-			file = new File("byteArray.txt");
-			write(area, b);
-		}	
+		RandomAccessFile rafw = new RandomAccessFile(file, "rw"); //creates random access file
+		rafw.seek(area); //sets pointer in file to area
+		rafw.setLength(DiskSpace.size() + b.length); //pre-allocates space for writing
+		rafw.write(b); //writes the byte array to the file
+		rafw.close();
 	}
 	
 	public static byte[] read(long area) throws IOException{
-		RandomAccessFile rafr = new RandomAccessFile(file, "rw");
-		byte[] B = new byte[DiskSpace.size()]; //creates a new byte array
+		RandomAccessFile rafr = new RandomAccessFile(file, "r");
 		rafr.seek(area); //sets pointer in file to area needed
-		rafr.readFully(B); //reads from pointer to end of file and writes to byte array
+		byte[] B = new byte[1024]; //creates a new byte array
+		//reads from pointer to end of area and writes to byte array
+		for (int i=0; i < 1024; i++) {
+			B[i] = rafr.readByte();
+		}
 		rafr.close();
 		return B;
 	}
 	
 	//returns the size of the file
-	public static int size() {
-		int fileSize = (int) file.length();
+	public static int size() throws IOException {
+		@SuppressWarnings("resource")
+		RandomAccessFile rafs = new RandomAccessFile(file, "r");
+		int fileSize = (int) rafs.length();
 		return fileSize;	
 	}
 	
 	//checks if the file is empty, not sure if really needed
-	public static boolean isEmpty() {
+	public static boolean isEmpty() throws IOException {
 		if (DiskSpace.size() == 0) return true;
 		else return false;
 	}
